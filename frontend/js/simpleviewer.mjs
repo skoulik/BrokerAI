68bdd79682be24bf41fb7c87fe48a334308f5967
Sergey <sergey@neatvideo.com>
2024-06-07 20:49:29 +0930

    Method for placing a marker over the PDF view.

--- frontend/js/simpleviewer.mjs
+++ frontend/js/simpleviewer.mjs
@@ -37,6 +37,33 @@ eventBus.on("pagesinit", function () {
     pdfViewer.currentScaleValue = "page-width";
 });
 
+function _placeMarker(pageView, params) {
+    //pageView.outputScale['sx'], pageView.viewport.scale
+    params['offsetX'] /= pageView.viewport.rawDims['pageWidth'];
+    params['offsetY'] /= pageView.viewport.rawDims['pageHeight'];
+    if(!pageView.annotationEditor) {
+        let editorLayer = pageView.annotationEditorLayer.annotationEditorLayer;
+        editorLayer.pasteEditor(pdfjsLib.AnnotationEditorType.FREETEXT, {});
+        let editor = editorLayer.createAndAddNewEditor(params, false);
+        editor.contentDiv.innerHTML = "<div style=\"font-size:50px; transform: translate(-100%, -50%);\">&#11162;</div>";
+        pageView.annotationEditor = editor;
+        editorLayer.pasteEditor(0, {});
+    }
+    pageView.annotationEditor.x = params['offsetX'];
+    pageView.annotationEditor.y = params['offsetY'];
+    pageView.annotationEditor.fixAndSetPosition();
+}
+
+eventBus.on("annotationeditorlayerrendered", function (event) {
+    let pageView = event.source;
+    let page_num = event.pageNumber-1;
+    let pdfDocument = pageView.renderingQueue.pdfViewer.pdfDocument;
+    if(pdfDocument.pendingMarkers[page_num]) {
+        _placeMarker(pageView, pdfDocument.pendingMarkers[page_num]);
+        delete pdfDocument.pendingMarkers[page_num];
+    }
+});
+
 async function loadPDFtoViewer(url)
 {
     const loadingTask = pdfjsLib.getDocument({
@@ -46,9 +73,25 @@ async function loadPDFtoViewer(url)
         'enableXfa': false,
     });
     const pdfDocument = await loadingTask.promise;
+    pdfDocument.pendingMarkers = {};
     pdfViewer.setDocument(pdfDocument);
     pdfLinkService.setDocument(pdfDocument, null);
 }
 
-globalThis.pdfViewer = pdfViewer;
+function placeMarker(page_num, params)
+{
+    let pageView = pdfViewer._pages[page_num];
+    if(pageView && pageView.annotationEditorLayer && pageView.annotationEditorLayer.annotationEditorLayer)
+        _placeMarker(pageView, params);
+    else
+       pdfViewer.pdfDocument.pendingMarkers[page_num] = params;
+}
+
+function scrollToPage(page_num) {
+    globalThis.pdfViewer.currentPageNumber = page_num+1;
+}
+
+globalThis.pdfViewer       = pdfViewer;
 globalThis.loadPDFtoViewer = loadPDFtoViewer;
+globalThis.scrollToPage    = scrollToPage;
+globalThis.placeMarker     = placeMarker;
